6c51bb5592424d827f06cf39594b3d18f2afdc2a
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2024-03-19 16:30:56 -0300

    axios-pokemon-api

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,20 +1,20 @@
 import { useState , useEffect} from "react";
+import axios from "axios";
 export function Example() {
     const [loading, setLoading] = useState(true)
     const [click, setClick] = useState(false)
     const [data, setData] = useState([])
     const fetchPokemon = () =>{
         setLoading(true)
-        fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
-          .then(response => {
-            return response.json();
-        }).then(data => {
-            console.log(data);
-            setData(data.results)
+        axios.get(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+        .then(response => {
+            console.log(response.data)
+            setData(response.data.results)
             setLoading(false)
         }).catch(err=>{
             console.log(err);
-        });}
+        })
+    }
     useEffect(() => {
         if(click){
             fetchPokemon()
